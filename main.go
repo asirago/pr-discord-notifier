4215cb98c73677d73b0160f7bf59783252c9959e
asirago <40822894+asirago@users.noreply.github.com>
2024-02-05 20:08:13 +0100

    feature/ added basic slash commands for now
    slash commands are being sent to discord as intended. Next is to add more
    more slash commands with more functionality.

--- main.go
+++ main.go
@@ -22,8 +22,9 @@ type application struct {
 type config struct {
 	Port        int    `mapstructure:"port"`
 	Token       string `mapstructure:"token"`
-	ChannelID   string `mapstructure:"channelid"`
-	Environment string `mapstructure:"environment"`
+	ChannelID   string `mapstructure:"channel"`
+	GuildID     string `mapstructure:"guild"`
+	Environment string `mapstructure:"env"`
 	Repo        string `mapstructure:"repo"`
 }
 
@@ -36,8 +37,10 @@ func main() {
 
 	pflag.IntVar(&app.cfg.Port, "port", 6666, "HTTP port")
 	pflag.StringVar(&app.cfg.Token, "token", "", "Discord bot token")
-	pflag.StringVar(&app.cfg.ChannelID, "channelID", "", "Discord channel id")
-	pflag.StringVar(&app.cfg.Environment, "env", "dev", "dev | prod")
+	pflag.StringVar(&app.cfg.ChannelID, "channel", "", "Discord channel id")
+	pflag.StringVar(&app.cfg.GuildID, "guild", "", "Discord guild (server) id")
+	pflag.StringVar(&app.cfg.Repo, "repo", "", "Link to GitHub repo with the pull requests")
+	pflag.StringVar(&app.cfg.Environment, "environment", "dev", "dev | prod")
 
 	configFileName := pflag.String("config", "", "config file name")
 
@@ -45,8 +48,9 @@ func main() {
 
 	viper.BindPFlag("port", pflag.Lookup("port"))
 	viper.BindPFlag("token", pflag.Lookup("token"))
-	viper.BindPFlag("channelID", pflag.Lookup("channelID"))
-	viper.BindPFlag("environment", pflag.Lookup("env"))
+	viper.BindPFlag("channel", pflag.Lookup("channel"))
+	viper.BindPFlag("guild", pflag.Lookup("guild"))
+	viper.BindPFlag("environment", pflag.Lookup("environment"))
 
 	if *configFileName != "" {
 		err := app.setupConfigFile(*configFileName)
@@ -55,12 +59,75 @@ func main() {
 		}
 	}
 
+	fmt.Printf("%+v\n\n", app.cfg)
+
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "repo",
+			Description: "Link to GitHub repository",
+		},
+		{
+			Name:        "echo",
+			Description: "Replies with your input",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type: discordgo.ApplicationCommandOptionString,
+					Name: "input",
+
+					Description: "The input to echo back",
+					Required:    true,
+				},
+			},
+		},
+	}
+
+	commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"repo": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: app.cfg.Repo,
+				},
+			})
+		},
+		"echo": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			input := i.ApplicationCommandData().Options[0].StringValue()
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: input,
+				},
+			})
+		},
+	}
+
 	dgo, err := discordgo.New("Bot " + app.cfg.Token)
 	if err != nil {
 		app.log.Fatal().Err(err).Msg("Failed to create discord session")
 	}
+	defer dgo.Close()
+
+	dgo.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(dgo, i)
+		}
+	})
+
+	err = dgo.Open()
+	if err != nil {
+		app.log.Fatal().Err(err).Msg("Error opening websocket connection")
+	}
+
+	fmt.Println("Bot is now running. Press Ctrl+C to exit")
 
 	app.dgo = dgo
+
+	for _, v := range commands {
+		_, err := app.dgo.ApplicationCommandCreate(app.dgo.State.User.ID, app.cfg.GuildID, v)
+		if err != nil {
+			app.log.Fatal().Err(err).Msg("Discord could not send command")
+		}
+	}
 
 	app.server()
 }
@@ -69,7 +136,6 @@ func (app *application) setupConfigFile(filename string) error {
 	viper.SetConfigName(filename)
 	viper.SetConfigType("toml")
 	viper.AddConfigPath(".")
-	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
 		app.log.Warn().Msg("config file could not be found")
